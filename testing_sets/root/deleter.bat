4cfc57ee384ef91daaf50343a574d94d319ab936
li.jon.seow.dev@gmail.com <li.jon.seow.dev@gmail.com>
2020-11-14 10:57:49 +0800

    updating test folders

--- testing_sets/root/deleter.bat
+++ testing_sets/root/deleter.bat
@@ -1,8 +1,7 @@
 set src_folder=D:\Users\Jon\Documents\Projects\Software\WindowsBAT\BatchDeleter
 set /p src_folder=Enter the source folder :
 echo you entered path as %src_folder%
-D:
-cd %src_folder%
+CD /D %src_folder%
 for /f "tokens=*" %%i in (deletefiles.txt) DO (
     del "%src_folder%\%%i"
 )
